686f739cc33d0ced91b3881837cac4d0bb3216c1
andreeacojocaru731 <gabrielaandreea25@yahoo.com>
2017-01-11 23:27:46 +0200

    Update led_temp.py
    Se salveaza temperatura la momentul pornirii aplicatiei ca si temperatura initiala ( initialTemp ).
    Se verifica temperatura curenta cu valoarea initiala, iar in functie de diferenta dintre ele se va aprinde ledul intr-o anumita culoare.
    Initial va fi verde, urmand sa varieze in nuante de albastru daca temperatura ambientala scade, sau in nuante de la galben->portocaliu->rosu daca temperatura e mai mare decat cea initiala.
    In acest caz, am ales ca si treapta, valoarea de 0.5 gradeC.

--- led_temp.py
+++ led_temp.py
@@ -13,15 +13,28 @@ LED_INVERT = False
 LED_STRIP = ws.SK6812_STRIP
 LED_CHANNEL = 0
 
-COLOR_LIST = [Color(255,0,0),Color(255,0,0),Color(0,255,0)]
+COLOR_LIST = [Color(0,0,255),Color(179,0,230),Color(255,0,0),Color(255,255,0),Color(204,255,0),Color(102,255,0),Color(0,255,0)]
+
+#Color(  G,  R,  B)
+		
+#Color(  0,  0,255), 0 - blue
+#Color(179,  0,230), 1 - light blue
+#Color(255,  0,  0), 2 - green
+#Color(255,255,  0), 3 - yellow
+#Color(204,255,  0), 4 - ocru
+#Color(102,255,  0), 5 - orange
+#Color(  0,255,  0), 6 - red
 
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
 strip.begin()
+
+print ('Ctrl-C to quit !')
 
 device_file = "/sys/bus/w1/devices/28-0000075f800d/w1_slave"
 
 
 def readTemperature():
+
         file = open(device_file, "r")
         data = file.read()
         file.close()
@@ -33,19 +46,32 @@ def readTemperature():
 
         return temperature
 
-while True:
-        #strip.setPixelColor(0,Color(255,0,0)) #Green
-        #strip.setPixelColor(0,Color(0,255,0)) #Red
-        #strip.setPixelColor(0,Color(0,0,255)) #Blue
+def changeColor(index):
+        strip.setPixelColor(0,COLOR_LIST[index])
+        strip.show()
+        time.sleep(0.1)
 
+        return
+
+initialTemp = readTemperature()
+
+while True:
 
         temperature = readTemperature()
 
-        if (temperature > 23.0) is True :
-                strip.setPixelColor(0,COLOR_LIST[2])
-                strip.show()
-                time.sleep(0.1)
-        else:
-                strip.setPixelColor(0,COLOR_LIST[1])
-                strip.show()
-                time.sleep(0.1)
+
+	if ((temperature >= (initialTemp - 0.5) ) and (temperature <= (initialTemp + 0.5) ))  is True :
+                changeColor(2)
+        elif (temperature < (initialTemp - 1)) is True :
+                changeColor(0)
+        elif (temperature < (initialTemp - 0.5)) is True :
+                changeColor(1)
+        elif (temperature > (initialTemp + 2)) is True :
+                changeColor(6)
+        elif (temperature > (initialTemp + 1.5)) is True :
+                changeColor(5)
+        elif (temperature > (initialTemp + 1)) is True :
+                changeColor(4)
+        elif (temperature > (initialTemp + 0.5)) is True :
+                changeColor(3)
+
